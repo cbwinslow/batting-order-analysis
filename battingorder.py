0d19ea7146bfb820c3858cbce09041c15d7c8777
drewscottt <19andrew.scott@boisecshools.net>
2021-12-25 23:01:31 -0700

    broke up code

--- battingorder.py
+++ battingorder.py
@@ -12,6 +12,8 @@
             python3 battingorder.py -lf <lineup filename> -of <outcomes filename>
 '''
 
+from Lineup import Lineup, Player
+
 import random
 from typing import List, Tuple
 import itertools
@@ -24,61 +26,6 @@ from statistics import mean
 import tqdm
 import heapq
 
-def get_player_ratio(stats: dict) -> dict:
-    '''
-        Returns a dict containing only the possible plate appearance outcomes for the input player
-    '''
-    ratios = {}
-
-    total = stats['b_total_pa']
-
-    total_r = 0
-    count = 0
-    ignore_fields = ['b_total_pa', 'first_name', 'last_name', 'player_id', 'year', 'slg_percent', 'on_base_percent', 'on_base_plus_slg']
-    for key, val in stats.items():
-        if key in ignore_fields:
-            ratios[key] = val
-            continue
-
-        r = val / total
-        ratios[key] = r
-
-        count += val
-        total_r += r
-
-
-    return ratios
-
-def get_stats(stats: str, col_names: str) -> dict:
-    '''
-        Returns a dict of all a player's stats
-    '''
-    d = {}
-
-    stats = stats[: -1].split(',')
-    stats = [int(s) if s.isnumeric() else s for s in stats]
-
-    col_names = col_names[:-1].split(',')
-
-    for col, stat in zip(col_names, stats):
-        d[col] = stat
-
-    return d
-
-def get_nine(total_count : int) -> List[int]:
-    '''
-        Returns a list of length 9 with unique indexes in the range of total_count
-    '''
-    nine = []
-
-    while len(nine) < 9:
-        r = random.randint(0, total_count - 1)
-
-        if r not in nine:
-            nine.append(r)
-
-    return nine
-
 def new_batter(cur_batter : int, thru_order:int) -> int:
     '''
         Returns the index of the next batter
@@ -88,35 +35,12 @@ def new_batter(cur_batter : int, thru_order:int) -> int:
 
     return cur_batter + 1, thru_order
 
-def get_PA_thresholds(player_stat : dict, outcomes : List[str]) -> List[float]:
-    '''
-        Returns the upper threshold (between 0 and 1) for each outcome. Used to decide a plate appearance outcome using a random number
-    '''
-    thresholds = []
-
-    thresholds.append(player_stat[outcomes[0]])
-    for i, outcome in enumerate(outcomes[1:], start=1):
-        thresholds.append(thresholds[i-1] + player_stat[outcome])
-
-    return thresholds
-
-def get_PA_outcome(threshold : List[float], outcomes : List[str]) -> str:
-    '''
-        Returns the name of the plate appearance outcome based on a randomly generated number
-    '''
-    rand_outcome = random.random()
-    for out_threshold, outcome in zip(threshold, outcomes):
-        if rand_outcome < out_threshold:
-            return outcome
-
-    return outcomes[-1]
-
 def sim_inning(generated_outcomes:dict, leadoff : int, thru_order:int, order:List[int]) -> Tuple[int, int]:
     '''
         Simulates an inning of play
         Returns the number of runs scored in the inning and what batter will lead off the next inning
     '''
-#    print('**** NEW INNING ****')
+
     runs = 0
     cur_batter_pos = leadoff
     cur_batter = order[cur_batter_pos]
@@ -139,7 +63,6 @@ def sim_inning(generated_outcomes:dict, leadoff : int, thru_order:int, order:Lis
             # and double/triple plays
             outs += 1
             if outs == 3:
-#                print(outcome, runs, cur_batter, thru_order)
                 # end of inning, no sacs possible
                 break
 
@@ -210,8 +133,6 @@ def sim_inning(generated_outcomes:dict, leadoff : int, thru_order:int, order:Lis
         else:
             raise Exception(f"Invalid outcome name: {outcome}")
 
-#        print(outcome, runs, cur_batter, thru_order)
-
         cur_batter_pos, thru_order = new_batter(cur_batter_pos, thru_order)
         cur_batter = order[cur_batter_pos]
 
@@ -219,7 +140,7 @@ def sim_inning(generated_outcomes:dict, leadoff : int, thru_order:int, order:Lis
 
     return runs, cur_batter_pos, thru_order
 
-def sim_order(order:List[int], per_order:int, generated_outcomes:List[dict]) -> float:
+def sim_order(order:List[int], per_order:int, lineup:Lineup) -> float:
     '''
         Simulates per_order games for each order
         Parallelized using imap
@@ -228,7 +149,7 @@ def sim_order(order:List[int], per_order:int, generated_outcomes:List[dict]) -> 
     for game_num in range(per_order):
         leadoff = 0
         thru_order = 0
-        game_outcomes = generated_outcomes[game_num]
+        game_outcomes = lineup.game_outcomes[game_num]
         for inning in range(9):
             runs, leadoff, thru_order = sim_inning(game_outcomes, leadoff, thru_order, order)
 
@@ -238,19 +159,16 @@ def sim_order(order:List[int], per_order:int, generated_outcomes:List[dict]) -> 
 
     return avg_runs_order
 
-def run_sim(player_ratios : dict, generated_outcomes:List[dict], per_order : int = 1) -> None:
+def run_sim(lineup:Lineup, per_order:int = 1) -> None:
     '''
         Simulates per_order games for each possible batting order
     '''
 
     orders = list(itertools.permutations([i for i in range(9)]))
-#    orders = [[5,8,0,1,4,6,7,2,3]]
-#    orders = [[0,1,3,5,4,2,6,8,7]]
-#    orders = [[0,1,3,5,7,4,6,8,2]]
     # run the simulation with the at bat outcomes
     pool = Pool(10)
     avg_runs_per_order = list(tqdm.tqdm( \
-        pool.imap(partial(sim_order, per_order=per_order, generated_outcomes=generated_outcomes), orders), \
+        pool.imap(partial(sim_order, per_order=per_order, lineup=lineup), orders), \
         total=len(orders)))
     pool.close()
     pool.join()
@@ -278,7 +196,7 @@ def run_sim(player_ratios : dict, generated_outcomes:List[dict], per_order : int
         order = orders[best_five[rank][1]] 
         print(f'{str(rank+1)}) Average runs for order: {best_five[rank][0]}')
         for i, ind in enumerate(order):
-            print(f'\t{str(i+1)}) {player_summary(player_ratios[ind])}')
+            print(f'\t{str(i+1)}) {str(lineup.players[ind])}')
         print()
 
     print(f'Bottom 5 batting orders:')
@@ -286,11 +204,63 @@ def run_sim(player_ratios : dict, generated_outcomes:List[dict], per_order : int
         order = orders[worst_five[rank][1]] 
         print(f'{str(rank+1)}) Average runs for order: {worst_five[rank][0]}')
         for i, ind in enumerate(order):
-            print(f'\t{str(i+1)}) {player_summary(player_ratios[ind])}')
+            print(f'\t{str(i+1)}) {str(lineup.players[ind])}')
         print()
 
-def player_summary(player : dict) -> str:
-    return f'{player["first_name"]} {player["last_name"]}: {player["on_base_percent"]} OBP, {player["slg_percent"]} SLG, {player["on_base_plus_slg"]} OPS'
+def get_nine(total_count : int) -> List[int]:
+    '''
+        Returns a list of length 9 with unique indexes in the range of total_count
+    '''
+    nine = []
+
+    while len(nine) < 9:
+        r = random.randint(0, total_count - 1)
+
+        if r not in nine:
+            nine.append(r)
+
+    return nine
+
+def get_lineup(lineup_filename:str) -> Lineup:
+    lineup = Lineup()
+
+    if lineup_filename is not None:
+        # get the players specified in the input file
+        player_names = []
+        with open(lineup_filename, 'r') as f_players:
+            for line in f_players:
+                first, last = line.split()
+                player_names.append(f'{last},{first}')
+
+        # read the player data from master file
+        with open('stats.csv', 'r', encoding='utf-8-sig') as stats_csv:
+            col_names = stats_csv.readline().strip()[:-1].split(',')
+            for line in stats_csv:
+                first_comma = line.index(',')
+                second_comma = line[first_comma + 1: ].index(',')
+                name = line[ : second_comma + first_comma + 1]
+
+                if name in player_names:
+                    lineup.add_player(Player(line))            
+
+    else:
+        # get all of the players
+        players = []
+        with open('stats.csv', 'r', encoding='utf-8-sig') as stats_csv:
+            col_names = stats_csv.readline().strip()[:-1].split(',')
+            for line in stats_csv:
+                players.append(Player(line))
+
+            # select 9 random players to run the sim
+            player_indexes = get_nine(len(ratios))
+            for index in player_indexes:
+                lineup.add_player(players[index])
+
+    # TODO: should this check be inside the Lineup class?
+    if len(lineup.players) != 9:
+        raise Exception(f'Incorrect number of players: {len(lineup.players)}, {lineup.players}')
+
+    return lineup
 
 def parse_arguments(args):
     '''
@@ -317,86 +287,6 @@ def parse_arguments(args):
 
     return lineup_filename, outcome_filename, sims_per_order
 
-def get_players(lineup_filename:str) -> List[dict]:
-    players = [None] * 9
-
-    if lineup_filename is not None:
-        # get the players specified in the input file
-        lineup = []
-        with open(lineup_filename, 'r') as f_players:
-            for line in f_players:
-                first, last = line.split()
-                lineup.append(f'{last},{first}')
-
-        # read the player data from master file
-        with open('stats.csv', 'r', encoding='utf-8-sig') as stats_csv:
-            col_names = stats_csv.readline().strip()
-
-            for line in stats_csv:
-                first_comma = line.index(',')
-                second_comma = line[first_comma + 1: ].index(',')
-                name = line[ : second_comma + first_comma + 1]
-
-                if name in lineup:
-                    stats = get_stats(line.strip(), col_names)
-
-                    player_ratio = get_player_ratio(stats)
-                    players[lineup.index(name)] = player_ratio
-
-    else:
-        # get all of the players
-        ratios = []
-        with open('stats.csv', 'r', encoding='utf-8-sig') as stats_csv:
-            col_names = stats_csv.readline().strip()
-
-            for line in stats_csv:
-                stats = get_stats(line.strip(), col_names)
-
-                player_ratio = get_player_ratio(stats)
-                ratios.append(player_ratio)
-
-            # select 9 random players to run the sim
-            player_indexes = get_nine(len(ratios))
-            players = [ratios[p] for p in player_indexes]
-
-    return players
-
-def generate_at_bat_outcomes(outcome_filename:str, per_order:int, player_ratios:dict) -> List[dict]:
-    generated_outcomes = []
-
-    if outcome_filename is None:
-        outcomes = ['b_single', 'b_double', 'b_triple', 'b_home_run', 'b_strikeout', 'b_walk', 'b_catcher_interf', 'b_hit_by_pitch', 'b_out_fly', 'b_out_ground', 'b_out_line_drive', 'b_out_popup']
-        player_thresholds = [get_PA_thresholds(ratio, outcomes) for ratio in player_ratios]
-
-        # pre-generate 10 at bats for each player per_order times
-        # this way, the outcomes are held constant across batting orders, so variance should fall
-        for _ in range(per_order):
-            game_outcomes = {}
-            for i, player in enumerate(player_ratios):
-                game_outcomes[i] = []
-                for _ in range(10):
-                    outcome = get_PA_outcome(player_thresholds[i], outcomes)
-                    game_outcomes[i].append(outcome)
-
-            generated_outcomes.append(game_outcomes)
-    else:
-        game_outcomes = {}
-        for i, player in enumerate(player_ratios):
-            game_outcomes[i] = []
-            player_outcomes = []
-            with open(outcome_filename, 'r') as outcome_f:
-                player_name = player['first_name'] + ' ' + player['last_name']
-                for line in outcome_f:
-                    if line[:len(player_name)] == player_name:
-                        player_outcomes = line[:-1].split(':')[1].split(',')
-
-            for outcome in player_outcomes:
-                game_outcomes[i].append(outcome)
-
-        generated_outcomes.append(game_outcomes)
-
-    return generated_outcomes
-
 if __name__ == '__main__':
     start = timeit.default_timer()
     players = []
@@ -411,20 +301,21 @@ if __name__ == '__main__':
         stats_csv.write(new_content)
 
     lineup_filename, outcome_filename, sims_per_order = parse_arguments(sys.argv[1:])
-
-    players = get_players(lineup_filename)
+    lineup = get_lineup(lineup_filename)
     
     print("Players:")
-    for player in players:
-        print(player_summary(player))
+    for player in lineup.players:
+        print(player)
     print()
 
-    assert len(players) == 9
-
-    outcomes = generate_at_bat_outcomes(outcome_filename, sims_per_order, players)
+    lineup.set_pa_outcomes(outcome_filename, sims_per_order)
 
     # set per_order to be the number of simulations to run per order
-    run_sim(players, outcomes, per_order=sims_per_order)
+    run_sim(lineup, per_order=sims_per_order)
 
     end = timeit.default_timer()
-    print(f'\nTotal time: {end - start:.2f} seconds')
+    seconds = int(end - start)
+    hours = seconds // 3600
+    minutes = (seconds - (hours * 3600)) // 60
+    seconds = seconds - (hours * 3600) - (minutes * 60)
+    print(f'Total time: {hours}:{minutes:02d}:{seconds:02d}')
