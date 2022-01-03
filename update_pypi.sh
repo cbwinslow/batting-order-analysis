d2a8ca6662789e2430d5028c60f4f388aee28d45
drewscottt <19andrew.scott@boisecshools.net>
2022-01-02 17:09:16 -0700

    update script

--- update_pypi.sh
+++ update_pypi.sh
@@ -1,3 +1,17 @@
-rm -rf dist
-python3 -m build
-twine upload dist/*
+#!/bin/bash
+
+# Adds, commits, and pushes all changes, then uploads to PyPI
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Commit message required"
+else
+    # TODO: try changing version in setup.cfg
+
+    git add .
+    git commit -m "$1"
+    git push origin main
+
+    rm -rf dist
+    python3 -m build
+    twine upload dist/*
+fi
